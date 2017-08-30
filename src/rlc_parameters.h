dc4ad0cd5a265d617816682bfc20909e5bff4285
Mikko Markus Torni <mikko.torni@aalto.fi>
2017-08-30 17:21:50 +0300

    Remove random C code from end of rlc_parameters.h

--- src/rlc_parameters.h
+++ src/rlc_parameters.h
@@ -29,34 +29,3 @@ const char *const rlc_parameter_names[] =
   NULL
 };
 
-
-  const char *const rlc_parameter_names[];
-  
-  /******** RLC API *********/
-  typedef struct rlc_state RLC;
-  RLC *   rlc_init();
-  void    rlc_free(RLC *state);
-  // rlc_set_mode resets all protocol state, but leaves parameters and callbacks untouched
-  // Returns -1 if parameters are invalid and sets errno. Mode is then undefined
-  int     rlc_reset();
-  int     rlc_set_parameters(RLC *state, const char *envz, size_t envz_len);
-  /* Returns -1 if doesn't want to or can't send a packet */
-  int     rlc_pdu_send_opportunity(RLC *state, unsigned time_in_ms, void *buffer, int size);
-  void    rlc_pdu_received(RLC *state, unsigned time_in_ms, void *buffer, int size);
-  // rlc_timer_tick: Use this to do slow work. Call radio_link_failure
-  // callback for example or shuffle buffers
-  void    rlc_timer_tick(RLC *state, unsigned time_in_ms);
-
-  /******** Callbacks *********/
-
-  // Implement these upper layer callbacks.
-
-  /* Return -1 if don't want to send a packet */
-  int  rlc_sdu_send_opportunity(void *arg, unsigned time_in_ms, void *buffer, size_t size);
-  void rlc_sdu_received(void *arg, unsigned time_in_ms, void *buffer, size_t size);
-  void rlc_sdu_delivered(void *arg, unsigned time_in_ms, void *buffer, size_t size);
-  void rlc_sdu_radio_link_failure(void *arg, unsigned time_in_ms);
-
-#ifdef __cplusplus
-}
-#endif			 
